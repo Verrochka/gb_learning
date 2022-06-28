eceaff736fd4f96800941163e305e4b78d455b66
Vera <vera.checheleva@gmail.com>
2022-06-28 16:10:55 +0300

    Added zadacha_4

---
+++ seminar_1/zadacha_4/zadacha_4.cs
@@ -0,0 +1,29 @@
+int number1 = 2;
+int number2 = 3;
+int number3 = 7;
+int max_number = 0;
+if (number1 > max_number)
+{
+    max_number = number1;
+}
+else 
+{
+    
+}
+if (number2 > max_number)
+{
+    max_number = number2;
+}
+else
+{
+    
+}
+if (number3 > max_number)
+{
+    max_number = number3;
+}
+else
+{
+    
+}
+Console.WriteLine($"max = {max_number}");
