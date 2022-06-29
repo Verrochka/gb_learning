44f445c3921b627d1e22170d919d1163f1100fde
Verrochka <107298466+Verrochka@users.noreply.github.com>
2022-06-29 10:30:16 +0300

    Update zadacha_4.cs

--- seminar_1/zadacha_4/zadacha_4.cs
+++ seminar_1/zadacha_4/zadacha_4.cs
@@ -1,29 +1,13 @@
 int number1 = 2;
 int number2 = 3;
 int number3 = 7;
-int max_number = 0;
-if (number1 > max_number)
-{
-    max_number = number1;
-}
-else 
-{
-    
-}
+int max_number = number1;
 if (number2 > max_number)
 {
     max_number = number2;
 }
-else
-{
-    
-}
 if (number3 > max_number)
 {
     max_number = number3;
-}
-else
-{
-    
 }
 Console.WriteLine($"max = {max_number}");
