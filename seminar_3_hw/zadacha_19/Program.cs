1d323f56ad4cf0046aa0f5ed2cbae618050d750c
Verrochka <107298466+Verrochka@users.noreply.github.com>
2022-07-04 13:34:19 +0300

    Add files via upload

---
+++ seminar_3_hw/zadacha_19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Здесь важный момент, как перевести число в массив одной строчкой. Строка 6.
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+var digitArray = number.ToString().Select(digit => int.Parse(digit.ToString())).ToArray();
+int n = digitArray.Length;
+
+if (n == 5 && digitArray[0] == digitArray[4] && digitArray[1] == digitArray[3])
+{
+    Console.WriteLine("Это число является палиндромом, т.е. оно читается одинаково справа налево и слева направо");
+}
+
+else if (n == 5 && digitArray[0] != digitArray[4] || digitArray[1] != digitArray[3])
+{
+    Console.WriteLine("Это число не является палиндромом");
+}
+
+else if (n != 5)
+{
+    Console.WriteLine("Вы ввели неверное число. Подсказка: пятизначное число - это когда в числе содержится 5 цифр)");
+}
---
+++ seminar_3_hw/zadacha_21/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+void print3Ddistance(double aX, double aY, double aZ, double bX, double bY, double bZ)
+{
+    double deltaX = aX - bX;
+    double deltaY = aY - bY;
+    double deltaZ = aZ - bZ;
+    double c = Math.Sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
+    Console.WriteLine($"Расстояние между точками в 3D пространстве равно {c}");
+}
+
+print3Ddistance(3, 6, 8, 2, 1, -7);
