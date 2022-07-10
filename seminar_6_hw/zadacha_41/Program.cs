1b8a8b0faae45f2b99e1c5456df4ec20978f217c
Verrochka <107298466+Verrochka@users.noreply.github.com>
2022-07-10 18:34:45 +0300

    Add files via upload

---
+++ seminar_6_hw/zadacha_41/Program.cs
@@ -0,0 +1,37 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.Console.Write ("Сколько чисел вы хотите ввести? Напишите: ");
+
+Console.Write("Сколько чисел вы хотите ввести? Напишите: ");
+int M = int.Parse(Console.ReadLine());
+int[] array = new int[M];
+
+Console.WriteLine("\nДалее мы попросим вас ввести числа. Помните, они могут быть как положительными, так и отрицательными.");
+Console.WriteLine("Следите, чтобы они не были дробными. Принимаются только целые числа");
+
+// не получилось код с 12 по 22 строчки сделать функцией(( - пишет, что там что-то куда-то не возвращает
+// int[] KeyboardInput(int number) - вот так пыталась
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1) 
+        {
+        Console.Write("\nВведите последнее число: ");
+        array[i] = int.Parse(Console.ReadLine());
+        break;
+        }
+    Console.Write("\nВведите число: ");
+    array[i] = int.Parse(Console.ReadLine());
+    }
+
+int PositiveNumbersCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write($"\nКоличество введенных вами положительных чисел равно {PositiveNumbersCount(array)}");
---
+++ seminar_6_hw/zadacha_43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите значение коэффициента b1: ");
+double b1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение коэффициента k1: ");
+double k1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение коэффициента b2: ");
+double b2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение коэффициента k2: ");
+double k2 = double.Parse(Console.ReadLine());
+
+double[] array = {b1, k1, b2, k2};
+ 
+double x = (b1 - b2) / (k2 - k1);
+
+double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2, равна {x}, {y}");
+
